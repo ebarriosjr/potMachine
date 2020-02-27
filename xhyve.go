f85db99b2018393060072270811f2a10fe2c0d53
ebarrios <esteban.barrios@trivago.com>
2020-02-27 13:42:30 +0100

    Added logic to test folder existence and write files to disk and other

--- xhyve.go
+++ xhyve.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"os/exec"
+	"sync"
+)
+
+var (
+	xhyveIP string
 )
 
 func initializeXhyve(verbose bool) {
-	//mkdir ~/.pot/xhyve if it doesnt exists
+	potDirPath := getVagrantDirPath()
+	xhyveDirPath := potDirPath + "/xhyve"
+
+	if _, err := os.Stat(xhyveDirPath); os.IsNotExist(err) {
+		os.Mkdir(xhyveDirPath, 0664)
+	}
+
 	//Download from github respo xhyve.tar.zg -> ~/.pot/xhyve
+	fileURL := "https://golangcode.com/images/avatar.jpg"
+	tarPath := xhyveDirPath + "/xhyve.tar.gz"
+
+	if _, err := os.Stat(tarPath); os.IsNotExist(err) {
+		if err := downloadFile(tarPath, fileURL); err != nil {
+			fmt.Println("Error downloading tar file from github with err: ", err)
+			log.Fatal()
+		}
+	}
 	//untar xhyve.tar.gz into ~/.pot/xhyve
 	//Check if runfile exists
 	//Create run file
@@ -28,14 +54,18 @@ ACPI="-A"
 xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_HDD $UUID -f fbsd,$USERBOOT,$IMG,"$KERNELENV"
 `
 	//Write runfile to ~/.pot/xhyve/run.sh
+	//wg.Add()
+	var wg sync.WaitGroup
+	go netcat(&wg)
+
 	//Initializa xhyve vm
 	err := runXhyve()
 	if err != nil {
 		fmt.Println("Error creating xhyve vm with err: ", err)
 		return
 	}
-	//Get xhyve ip
-	xhyveIP := ""
+	wg.Wait()
+
 	//generate sshConfig file
 	sshConfig := `Host potMachine
   HostName ` + xhyveIP + `
@@ -48,9 +78,21 @@ xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_HDD $UUID -f fbsd,$USERBOOT,$I
   IdentitiesOnly yes
   LogLevel FATAL
 `
+	xhyveRunFilePath := potDirPath + "/xhyve/runFile.sh"
 
-	fmt.Println("Runfile", runFile)
-	fmt.Println("sshConfig", sshConfig)
+	err = ioutil.WriteFile(xhyveRunFilePath, []byte(runFile), 0664)
+	if err != nil {
+		fmt.Println("ERROR: Error writting file to disk with err: \n", err)
+		return
+	}
+
+	xhyvesshConfigFilePath := potDirPath + "/xhyve/sshConfig"
+
+	err = ioutil.WriteFile(xhyvesshConfigFilePath, []byte(sshConfig), 0664)
+	if err != nil {
+		fmt.Println("ERROR: Error writting file to disk with err: \n", err)
+		return
+	}
 }
 
 func runXhyve() error {
@@ -81,4 +123,41 @@ func runXhyve() error {
 		return err
 	}
 	return nil
+}
+
+func netcat(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	termCmd := "nc -l 1234"
+	cmd := exec.Command("bash", "-c", termCmd)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error getting ip information from the VM with err: ", err)
+		log.Fatal(err)
+	}
+	cmd.Wait()
+	xhyveIP = out.String()
+}
+
+func downloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
